e7a468f57af04702879c4fbc7dd5afe9260e3cd6
brianhsu7476 <brianhsu7476@gmail.com>
2024-09-05 17:05:20 +0800

    fix Inversion and Convex_hull

--- codebook/8_Geometry/Inversion.cpp
+++ codebook/8_Geometry/Inversion.cpp
@@ -1,7 +1,3 @@
-// center: o, power: r
-void inversion(vector<pdd> &dots, pdd o, double r) {
-  for(auto &x: dots) {
-    double dis = dot(x - o, x - o);
-    x = o + (x - o) * r / dis;
-  }
+void inversion(vector<pdd> &dots, pdd o, double r){ //center: o, power: r
+  for(auto &x:dots)x=o+(x-o)*r/dot(x-o, x-o);
 }
