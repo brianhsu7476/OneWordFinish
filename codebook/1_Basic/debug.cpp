8751367bf2cca158abdb5edacb249b3a9f25ce08
brianhsu7476 <brianhsu7476@gmail.com>
2024-09-27 14:39:03 +0800

    add debug macro and remove python

---
+++ codebook/1_Basic/debug.cpp
@@ -0,0 +1,12 @@
+#define all(x) begin(x), end(x)
+#ifdef OWF
+#define safe cerr<<__PRETTY_FUNCTION__<<" line "<<__LINE__<<" safe\n"
+#define debug(args...) kout("\e[1;32m[ " + string (#args) + " ]\e[0m", args)
+void kout() { cerr << endl; }
+template <class T, class ...U> void kout(T a, U ...b) { cerr << a << ' ',kout(b...); }
+template <class T> void pary(T L, T R) { while (L != R) cerr << *L << " \n"[++L==R]; }
+#else
+#define safe ((void)0)
+#define debug(...) safe
+#define pary(...) safe
+#endif
